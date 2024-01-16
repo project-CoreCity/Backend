945dc5cd2e750c5c751bd8fa46c0748e454429ca
Koreanerd <koreanerd@Koreanerds-MacBook-Air.local>
2024-01-16 10:00:37 +0900

    feat: Implement GET /api/v1/admin/users API Endpoint and getAccessRequestUserList Controller
    - Verifies the admin status of a user based on their UID and the target server address for dashboard access requests.
    - If the user is an admin, retrieves and sends a list of users who have requested access to the specified server address.
    - Returns a 'Access denied' message with a 403 response if the user does not have admin privileges.
    - Returns an empty array and avoids unnecessary database queries if the user ID list in the request is empty.
    - Provides a 404 response with appropriate error messages for non-existent users or server addresses.
    - Sends a general error message with a 500 response in case of internal server errors.
    
    This commit enhances dashboard management functionalities, allowing admin users to effectively manage access requests to the dashboard.

--- src/controllers/adminUser.js
+++ src/controllers/adminUser.js
@@ -1,3 +1,4 @@
+const User = require("../models/User");
 const ServerAddress = require("../models/ServerAddress");
 const UserServerRelation = require("../models/UserServerRelation");
 
@@ -6,7 +7,7 @@ const getPendingRequests = async (req, res) => {
 
   try {
     const requestPromises = addressIds.map((addressId) =>
-      UserServerRelation.find({ addressId, isAdmin: false }).lean(),
+      UserServerRelation.find({ addressId, isApproved: false }).lean(),
     );
     const addressPromises = addressIds.map((addressId) =>
       ServerAddress.findById(addressId).lean(),
@@ -33,6 +34,63 @@ const getPendingRequests = async (req, res) => {
   }
 };
 
+const getAccessRequestUserList = async (req, res) => {
+  const uid = req.user.uid;
+  const serverAddress = req.query.address;
+  const userIds = Array.isArray(req.query.userId)
+    ? req.query.userId
+    : [req.query.userId];
+
+  if (!userIds.length) {
+    return res.status(200).json({ userList: [] });
+  }
+
+  try {
+    const userInfo = await User.findOne({ uid });
+
+    if (!userInfo) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const serverInfo = await ServerAddress.findOne({ address: serverAddress });
+
+    if (!serverInfo) {
+      return res.status(404).json({ message: "Server address not found" });
+    }
+
+    const requestTarget = await UserServerRelation.findOne({
+      userId: userInfo._id,
+      addressId: serverInfo._id,
+    });
+
+    if (!requestTarget || !requestTarget.isAdmin) {
+      return res.status(403).json({ message: "Access denied" });
+    }
+
+    const requestUserPromises = userIds.map((userId) =>
+      User.findOne({ _id: userId }),
+    );
+
+    const requestUserResults = await Promise.all(requestUserPromises);
+
+    const requestUsersNameList = requestUserResults.map((user) => ({
+      name: user.name,
+    }));
+
+    res.status(200).json({
+      userList: requestUsersNameList,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      status: "Error",
+      message: "Server encountered an error processing the request.",
+    });
+  }
+};
+
 module.exports = {
   getPendingRequests,
+  getAccessRequestUserList,
 };
--- src/routes/adminUsers.js
+++ src/routes/adminUsers.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { verifyToken } = require("../middlewares/auth");
-const { getPendingRequests } = require("../controllers/adminUser");
+const {
+  getPendingRequests,
+  getAccessRequestUserList,
+} = require("../controllers/adminUser");
 
 router.route("/server-addresses/requests").get(verifyToken, getPendingRequests);
+
+router.route("/users").get(verifyToken, getAccessRequestUserList);
 
 module.exports = router;
