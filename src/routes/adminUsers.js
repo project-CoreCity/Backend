a2d7003b8fe4535fbe70845b34c57534e90c138c
Koreanerd <koreanerd@Koreanerds-MacBook-Air.local>
2024-01-24 02:27:10 +0900

    feat: Refactor Admin API Endpoints for Enhanced Structure and Clarity
    - Renamed  to  to more accurately reflect its functionality in fetching the list of approval requests for each server address.
    - Replaced  with  and updated the API path to .
    - Introduced new functionalities  and  to handle PATCH and DELETE operations for individual user requests for a specific server address.
    - Created new paths .
    
    This commit not only improves the readability and maintainability of our API endpoints but also ensures a more logical and systematic approach in managing server addresses and user request processing.

--- src/routes/adminUsers.js
+++ src/routes/adminUsers.js
@@ -2,12 +2,23 @@ const express = require("express");
 const router = express.Router();
 const { verifyToken } = require("../middlewares/auth");
 const {
-  getPendingRequests,
-  getAccessRequestUserList,
+  getApprovalRequestServerList,
+  getApprovalRequestUserList,
+  updateRequestApprovalStatus,
+  deleteApprovalRequest,
 } = require("../controllers/adminUser");
 
-router.route("/server-addresses/requests").get(verifyToken, getPendingRequests);
+router
+  .route("/server-addresses/requests")
+  .get(verifyToken, getApprovalRequestServerList);
 
-router.route("/users").get(verifyToken, getAccessRequestUserList);
+router
+  .route("/server-addresses/:addressId/users/requests")
+  .get(verifyToken, getApprovalRequestUserList);
+
+router
+  .route("/server-addresses/:addressId/requests/:userId")
+  .patch(verifyToken, updateRequestApprovalStatus)
+  .delete(verifyToken, deleteApprovalRequest);
 
 module.exports = router;
